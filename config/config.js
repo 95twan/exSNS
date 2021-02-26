61a018c4fa18ce6134afa6f28a8a5f8879cc318e
95twan <95twan@naver.com>
2021-02-26 20:58:32 +0900

    fix kakao callback url

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ module.exports = {
         username: 'root',
         password: process.env.SEQUELIZE_PASSWORD,
         database: 'nodeSNS',
-        host: process.env.RDS_HOST,
+        host: '127.0.0.1',
         dialect: 'mysql',
         operatorsAliases: false,
     },
--- passport/kakaoStrategy.js
+++ passport/kakaoStrategy.js
@@ -1,10 +1,13 @@
 const KakaoStrategy = require('passport-kakao').Strategy
 const { User } = require('../models');
 
+const clientID = process.env.NODE_ENV === 'production' ? process.env.KAKAO_ID : process.env.DEV_KAKAO_ID
+const callbackURL = process.env.NODE_ENV === 'production' ? 'http://13.209.35.12/auth/kakao/callback' : 'http://localhost:8001/auth/kakao/callback'
+
 module.exports = (passport) => {
     passport.use(new KakaoStrategy({
-        clientID: process.env.KAKAO_ID,
-        callbackURL: 'http://localhost/auth/kakao/callback',
+        clientID,
+        callbackURL,
     }, async (accessToken, refreshToken, profile, done) => {
         try {
             const exUser = await User.findOne({ where: { snsId: profile.id, provider: 'kakao' } });
